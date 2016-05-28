0dee4e74834e6a222b3eaa164dc9ee5ccbe93eef
shakti_sd <shakti.sd@gmail.com>
2016-05-28 10:20:52 +0530

    added mentions

--- src/main/java/CorefExample.java
+++ src/main/java/CorefExample.java
@@ -16,7 +16,7 @@ public class CorefExample {
     String input = new String(Files.readAllBytes(Paths.get("src/input.txt")));
     Annotation document = new Annotation(input);
     Properties props = new Properties();
-    props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,mention,dcoref");
+    props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,mention,dcoref,sentiment");
     //props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,dcoref");
     
     StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
